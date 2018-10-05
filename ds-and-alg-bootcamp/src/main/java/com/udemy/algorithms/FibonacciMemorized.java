559d4e8ec6dbb65acbe52d3d589fceb1f28e5024
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2018-10-05 20:59:14 +0300

    added fibinacci memorized

---
+++ ds-and-alg-bootcamp/src/main/java/com/udemy/algorithms/FibonacciMemorized.java
@@ -0,0 +1,24 @@
+package com.udemy.algorithms;
+
+/**
+ * @author vsushko
+ */
+public class FibonacciMemorized {
+    private static int[] memo = new int[1001];
+
+    public static void main(String[] args) {
+        System.out.println(fib(1000));
+    }
+
+    private static int fib(int n) {
+        System.out.println("n = " + n);
+        if (n <= 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else if (memo[n] == 0) {
+            memo[n] = fib(n - 1) + fib(n - 2);
+        }
+        return memo[n];
+    }
+}
