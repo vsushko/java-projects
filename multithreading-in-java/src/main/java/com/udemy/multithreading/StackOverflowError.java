185bb1fed0ce63fad33894b8dd90f46c2e59052c
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2018-11-02 17:58:28 +0300

    added stack overflow error

---
+++ multithreading-in-java/src/main/java/com/udemy/multithreading/StackOverflowError.java
@@ -0,0 +1,16 @@
+package com.udemy.multithreading;
+
+/**
+ * @author vsushko
+ */
+public class StackOverflowError {
+
+    public static void main(String[] args) {
+        new StackOverflowError().print(0);
+    }
+
+    public synchronized void print(int i) {
+        //System.out.println(i++);
+        print(i);
+    }
+}
