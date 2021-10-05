15b5edcc22661858045bb02ea2e2460445ac4e52
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2021-10-05 22:45:40 +0300

    refactorings

--- java-playground/src/main/java/vsushko/algorithms/sorting/BubbleSort.java
+++ java-playground/src/main/java/vsushko/algorithms/sorting/BubbleSort.java
@@ -9,23 +9,28 @@ import java.util.Arrays;
  */
 class BubbleSort {
 
-    public static void main(String args[]) {
-        int arr[] = {64, 34, 25, 12, 22, 11, 90};
-        bubbleSort(arr, arr.length);
+    public static void main(String[] args) {
+        int[] arr = {64, 34, 25, 12, 22, 11, 90};
+
+        bubbleSort(arr);
+
         System.out.println("Sorted array");
         System.out.println(Arrays.toString(arr));
     }
 
-    private static void bubbleSort(int[] arr, int n) {
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
+    private static void bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
-                    // swap temp and arr[i]
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
+                    swap(arr, j);
                 }
             }
         }
+    }
+
+    private static void swap(int[] arr, int j) {
+        int temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
     }
 }
