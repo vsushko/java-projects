fac2ee6eb1988d26f699ab82d10e333c1f6385ae
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2023-02-28 00:28:43 +0300

    removed test

--- parallel-asynchronous-using-java/src/main/java/com/learnjava/executor/ProductServiceUsingExecutor.java
+++ parallel-asynchronous-using-java/src/main/java/com/learnjava/executor/ProductServiceUsingExecutor.java
@@ -1,7 +1,56 @@
 package com.learnjava.executor;
 
+import com.learnjava.domain.Product;
+import com.learnjava.domain.ProductInfo;
+import com.learnjava.domain.Review;
+import com.learnjava.service.ProductInfoService;
+import com.learnjava.service.ReviewService;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import static com.learnjava.util.CommonUtil.stopWatch;
+import static com.learnjava.util.LoggerUtil.log;
+
 /**
  * @author vsushko
  */
 public class ProductServiceUsingExecutor {
+
+    private static final ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
+    private final ProductInfoService productInfoService;
+    private final ReviewService reviewService;
+
+    public ProductServiceUsingExecutor(ProductInfoService productInfoService, ReviewService reviewService) {
+        this.productInfoService = productInfoService;
+        this.reviewService = reviewService;
+    }
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ProductInfoService productInfoService = new ProductInfoService();
+        ReviewService reviewService = new ReviewService();
+        ProductServiceUsingExecutor productService = new ProductServiceUsingExecutor(productInfoService, reviewService);
+        String productId = "ABC123";
+        Product product = productService.retrieveProductDetails(productId);
+        log("Product is " + product);
+
+        executorService.shutdown();
+    }
+
+    public Product retrieveProductDetails(String productId) throws ExecutionException, InterruptedException {
+        stopWatch.start();
+
+        Future<ProductInfo> productInfoFuture = executorService.submit(() -> productInfoService.retrieveProductInfo(productId));
+        Future<Review> reviewFuture = executorService.submit(() -> reviewService.retrieveReviews(productId));
+
+        ProductInfo productInfo = productInfoFuture.get();
+        Review review = reviewFuture.get();
+
+        stopWatch.stop();
+        log("Total Time Taken : " + stopWatch.getTime());
+        return new Product(productId, productInfo, review);
+    }
 }
