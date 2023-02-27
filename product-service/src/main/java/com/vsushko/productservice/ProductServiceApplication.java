7b51a1339dfebfcec5422b211a5a894164ca4ee1
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2023-02-28 00:09:19 +0300

    added product service

---
+++ product-service/src/main/java/com/vsushko/productservice/ProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.vsushko.productservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServiceApplication {
+
+    public static void main(String[] args) {
+        // 47:48
+        SpringApplication.run(ProductServiceApplication.class, args);
+    }
+}
