bf19e70dd77a782a05d70a418d45c3c26deb0ae7
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2023-03-15 02:57:21 +0300

    added java-multithreading-concurrency-performance-optimization project

---
+++ java-multithreading-concurrency-performance-optimization/src/main/java/com/vsushko/Main.java
@@ -0,0 +1,21 @@
+package com.vsushko;
+
+/**
+ * Hello world!
+ */
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("We are no in thread " + Thread.currentThread().getName());
+            }
+        });
+        System.out.println("We are in thread: " + Thread.currentThread().getName() + " before starting a new thread");
+        thread.start();
+        System.out.println("We are in thread: " + Thread.currentThread().getName() + " after starting a new thread");
+
+        Thread.sleep(10000);
+    }
+}
