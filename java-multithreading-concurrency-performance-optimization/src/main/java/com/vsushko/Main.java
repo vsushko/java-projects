25683239eb21c069972329667f5dd41025609966
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2023-03-15 10:49:54 +0300

    added exception example

--- java-multithreading-concurrency-performance-optimization/src/main/java/com/vsushko/Main.java
+++ java-multithreading-concurrency-performance-optimization/src/main/java/com/vsushko/Main.java
@@ -9,9 +9,20 @@ public class Main {
         Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
-                System.out.println("We are no in thread " + Thread.currentThread().getName());
+                System.out.println("We are now in thread " + Thread.currentThread().getName());
+                System.out.println("Current thread priority is " + Thread.currentThread().getPriority());
+                // throw new RuntimeException("Internal error!");
             }
         });
+        thread.setName("New Worker Thread");
+        thread.setPriority(Thread.MAX_PRIORITY);
+        thread.setUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+                System.out.println("A critical error happened in thread " + t.getName() + " the error is " + e.getMessage());
+            }
+        });
+
         System.out.println("We are in thread: " + Thread.currentThread().getName() + " before starting a new thread");
         thread.start();
         System.out.println("We are in thread: " + Thread.currentThread().getName() + " after starting a new thread");
