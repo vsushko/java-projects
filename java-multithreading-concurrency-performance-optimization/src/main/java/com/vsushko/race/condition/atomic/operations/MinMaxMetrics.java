6f01843685e1b24009c3b000373d05602ee591c3
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2023-03-17 02:39:39 +0300

    threads synchronization

---
+++ java-multithreading-concurrency-performance-optimization/src/main/java/com/vsushko/race/condition/atomic/operations/MinMaxMetrics.java
@@ -0,0 +1,29 @@
+package com.vsushko.race.condition.atomic.operations;
+
+/**
+ * @author vsushko
+ */
+public class MinMaxMetrics {
+    private volatile long minValue;
+    private volatile long maxValue;
+
+    public MinMaxMetrics() {
+        this.maxValue = Long.MIN_VALUE;
+        this.minValue = Long.MAX_VALUE;
+    }
+
+    public void addSample(long newSample) {
+        synchronized (this) {
+            this.minValue = Math.min(newSample, this.minValue);
+            this.maxValue = Math.max(newSample, this.maxValue);
+        }
+    }
+
+    public long getMin() {
+        return this.minValue;
+    }
+
+    public long getMax() {
+        return this.maxValue;
+    }
+}
---
+++ java-multithreading-concurrency-performance-optimization/src/main/java/com/vsushko/synchronization/Main.java
@@ -0,0 +1,90 @@
+package com.vsushko.synchronization;
+
+/**
+ * @author vsushko
+ */
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+        InventoryCounter inventoryCounter = new InventoryCounter();
+        IncrementingThread incrementingThread = new IncrementingThread(inventoryCounter);
+        DecrementingThread decrementingThread = new DecrementingThread(inventoryCounter);
+
+        incrementingThread.start();
+        decrementingThread.start();
+
+        incrementingThread.join();
+        decrementingThread.join();
+
+        System.out.println("We currently have " + inventoryCounter.getItems() + " items");
+    }
+
+    public static class DecrementingThread extends Thread {
+        private final InventoryCounter inventoryCounter;
+
+        public DecrementingThread(InventoryCounter inventoryCounter) {
+            this.inventoryCounter = inventoryCounter;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 10000; i++) {
+                inventoryCounter.decrement();
+            }
+        }
+    }
+
+    public static class IncrementingThread extends Thread {
+        private final InventoryCounter inventoryCounter;
+
+        public IncrementingThread(InventoryCounter inventoryCounter) {
+            this.inventoryCounter = inventoryCounter;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 10000; i++) {
+                inventoryCounter.increment();
+            }
+        }
+    }
+
+    private static class InventoryCounter {
+        private int items = 0;
+
+        public synchronized void increment() {
+            items++;
+        }
+
+        public synchronized void decrement() {
+            items--;
+        }
+
+        public synchronized int getItems() {
+            return items;
+        }
+    }
+
+    private static class InventoryCounter2 {
+        private int items = 0;
+        private final Object lock = new Object();
+
+        public void increment() {
+            synchronized (this.lock) {
+                items++;
+            }
+        }
+
+        public void decrement() {
+            synchronized (this.lock) {
+                items--;
+            }
+        }
+
+        public int getItems() {
+            synchronized (this.lock) {
+                return items;
+            }
+        }
+    }
+}
