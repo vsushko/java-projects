3858d6b1a1f7b43c09481d02f8fdb64b69455b25
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2023-04-11 02:33:09 +0300

    securing the user command & query apis

--- master-spring-boot-microservices-with-cqrs-event-sourcing/user-management/user.cmd.api/src/main/java/com/springbank/user/cmd/api/controllers/UpdateUserController.java
+++ master-spring-boot-microservices-with-cqrs-event-sourcing/user-management/user.cmd.api/src/main/java/com/springbank/user/cmd/api/controllers/UpdateUserController.java
@@ -6,6 +6,7 @@ import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -30,6 +31,7 @@ public class UpdateUserController {
     }
 
     @PutMapping(path = "{id}")
+    @PreAuthorize("hasAuthority('WRITE_PRIVILEGE')")
     public ResponseEntity<BaseResponse> updateUser(@PathVariable(value = "id") String id,
                                                    @Valid @RequestBody UpdateUserCommand command) {
         try {
