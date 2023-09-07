16c8a752f8d45b522adb0104f1dbadafd210a418
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2023-09-07 16:33:18 +0300

    lates chages

--- spring-boot-spring-cloud-microservices/university-app/address-service/src/main/java/com/vsprog/cloud/addressservice/controller/AddressController.java
+++ spring-boot-spring-cloud-microservices/university-app/address-service/src/main/java/com/vsprog/cloud/addressservice/controller/AddressController.java
@@ -4,6 +4,8 @@ import com.vsprog.cloud.addressservice.request.CreateAddressRequest;
 import com.vsprog.cloud.addressservice.response.AddressResponse;
 import com.vsprog.cloud.addressservice.service.AddressService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,12 +16,16 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * @author vsushko
  */
+@RefreshScope
 @RestController
 @RequestMapping("/api/address")
 public class AddressController {
 
     @Autowired
     private AddressService addressService;
+
+    @Value("${address.test}")
+    private String test;
 
     @PostMapping("/create")
     public AddressResponse createAddress(@RequestBody CreateAddressRequest createAddressRequest) {
@@ -29,5 +35,10 @@ public class AddressController {
     @GetMapping("/getById/{id}")
     public AddressResponse getById(@PathVariable long id) {
         return addressService.getById(id);
+    }
+
+    @GetMapping("/test")
+    public String getTest() {
+        return test;
     }
 }
