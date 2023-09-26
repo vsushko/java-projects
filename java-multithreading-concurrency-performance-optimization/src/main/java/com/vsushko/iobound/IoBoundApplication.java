b346b94d684b9e386e89b096599f55f5937b4e91
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2023-09-26 10:17:59 +0300

    threading models for high performance io

--- java-multithreading-concurrency-performance-optimization/src/main/java/com/vsushko/iobound/IoBoundApplication.java
+++ java-multithreading-concurrency-performance-optimization/src/main/java/com/vsushko/iobound/IoBoundApplication.java
@@ -1,5 +1,46 @@
-package com.vsushko.iobound;/**
- * 
+package com.vsushko.iobound;
+
+import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
  * @author vsushko
- */public class IoBoundApplication {
+ */
+public class IoBoundApplication {
+
+    private static final int NUMBER_OF_TASKS = 10000;
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Press enter to start");
+        s.nextLine();
+        System.out.printf("Running %d tasks\n", NUMBER_OF_TASKS);
+
+        long start = System.currentTimeMillis();
+        performTasks();
+        System.out.printf("Tasks took %dms to complete\n", System.currentTimeMillis() - start);
+    }
+
+    private static void performTasks() {
+        ExecutorService executorService = Executors.newFixedThreadPool(1000);
+
+        for (int i = 0; i < NUMBER_OF_TASKS; i++) {
+            executorService.submit(() -> {
+                for (int j = 0; j < 100; j++) {
+                    blockingIoOperation();
+                }
+            });
+        }
+    }
+
+    private static void blockingIoOperation() {
+        System.out.println("Executing a blocking task from thread: " + Thread.currentThread());
+
+        try {
+            Thread.sleep(10);
+        } catch (InterruptedException e) {
+            throw new RuntimeException();
+        }
+    }
 }
--- java-multithreading-concurrency-performance-optimization/src/main/java/com/vsushko/iobound/OnlineStoreWebApplication.java
+++ java-multithreading-concurrency-performance-optimization/src/main/java/com/vsushko/iobound/OnlineStoreWebApplication.java
@@ -1,5 +1,43 @@
-package com.vsushko.iobound;/**
- * 
+package com.vsushko.iobound;
+
+/**
  * @author vsushko
- */public class OnlineStoreWebApplication {
+ */
+public class OnlineStoreWebApplication {
+    /*
+
+    private HttpClient httpClient;
+
+    // Starts an HTTP Server listening on port 8080.
+    // Delegates the handling of the requests to the handleHttpRequest method
+    private void startHttpServer() throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        HttpContext context = server.createContext("/");
+        context.setHandler(this::handleHttpRequest);
+
+        // UPDATE
+        ExecutorService executorService = Executors.newFixedThreadPool(8);
+        this.httpClient = HttpClient.newBuilder().executor(executorService).build();
+        server.setExecutor(executorService);
+
+        server.start();
+    }
+
+    //  UPDATED - Handles an incoming HTTP request from a user
+    private void handleHttpRequest(HttpExchange httpExchange) {
+        int numberOfProducts = parseRequest(httpExchange);
+        URI requestURI = URI.create(String.format("best-online-store/products?number-of-products=%d", numberOfProducts);
+
+        CompletableFuture<HttpResponse<String>> responseFuture =
+                httpClient.sendAsync(HttpRequest.newBuilder()
+                                .GET()
+                                .uri(requestURI)
+                                .build(),
+                        HttpResponse.BodyHandlers.ofString());
+
+        responseFuture.thenAccept( response -> {
+            sendWebpageToUser(httpExchange, response);
+        });
+    }
+    */
 }
