b2d8d0798f6b188181c3cc8011c3090d6f087c1e
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2023-12-24 12:47:14 +0400

    added example with deadline

--- grpc-java/src/main/java/com/vsushko/grpc/greeting/client/GreetingClient.java
+++ grpc-java/src/main/java/com/vsushko/grpc/greeting/client/GreetingClient.java
@@ -3,8 +3,11 @@ package com.vsushko.grpc.greeting.client;
 import com.proto.greeting.GreetingRequest;
 import com.proto.greeting.GreetingResponse;
 import com.proto.greeting.GreetingServiceGrpc;
+import io.grpc.Deadline;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.Status;
+import io.grpc.StatusRuntimeException;
 import io.grpc.stub.StreamObserver;
 
 import java.util.ArrayList;
@@ -103,6 +106,31 @@ public class GreetingClient {
         latch.await(3, TimeUnit.SECONDS);
     }
 
+    private static void doGreetWithDeadline(ManagedChannel channel) {
+        System.out.println("Enter doGreetWithDeadline");
+        GreetingServiceGrpc.GreetingServiceBlockingStub stub = GreetingServiceGrpc.newBlockingStub(channel);
+
+        GreetingResponse response = stub.withDeadline(Deadline.after(3, TimeUnit.SECONDS))
+                .greetWithDeadline(GreetingRequest.newBuilder()
+                        .setFirstName("Vasiliy")
+                        .build());
+
+        System.out.println("Greeting withing deadline: " + response.getResult());
+
+        try {
+            response = stub.withDeadline(Deadline.after(100, TimeUnit.MILLISECONDS))
+                    .greetWithDeadline(GreetingRequest.newBuilder().setFirstName("Vasiliy").build());
+            System.out.println("Greeting deadline exceeded: " + response.getResult());
+        } catch (StatusRuntimeException e) {
+            if (e.getStatus().getCode() == Status.Code.DEADLINE_EXCEEDED) {
+                System.out.println("Deadline has been exceeded");
+            } else {
+                System.out.println("Got an exception in greetWithDeadline");
+                e.printStackTrace();
+            }
+        }
+    }
+
     public static void main(String[] args) throws InterruptedException {
         if (args.length == 0) {
             System.out.println("Need one argument to work");
@@ -126,6 +154,9 @@ public class GreetingClient {
                 break;
             case "greet_everyone":
                 doGreetEveryone(channel);
+                break;
+            case "greet_with_deadline":
+                doGreetWithDeadline(channel);
                 break;
             default:
                 System.out.println("Keyword Invalid: " + args[0]);
